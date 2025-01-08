c4f1106ab2815b9a728c8b0a14c264c3d5645338
Luong Tu Nhan <tunhan281204@gmail.com>
2025-01-09 01:26:22 +0700

    fix: add app root

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+process.env.EXPO_ROUTER_APP_ROOT = process.env.EXPO_ROUTER_APP_ROOT || ".";
+
 const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts.push("cjs");
 
 module.exports = withNativeWind(config, { input: "./app/global.css" });
