8448f9bfcb43e609b17e6c2f79ef4b5a4910ecec
Luong Tu Nhan <tunhan281204@gmail.com>
2025-01-09 11:17:22 +0700

    ci: add new ci pipeline

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-process.env.EXPO_ROUTER_APP_ROOT = process.env.EXPO_ROUTER_APP_ROOT || ".";
-
 const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts.push("cjs");
 
 module.exports = withNativeWind(config, { input: "./app/global.css" });
